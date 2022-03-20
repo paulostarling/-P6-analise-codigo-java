9042645ca67646fad8979f98c804b6577f1ba3f5
Paulo Starling <ppstarling1@gmail.com>
2022-03-20 20:42:00 -0300

    Finishing sprint 01

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import numpy as np
 from git import Repo
 from datetime import date
 from datetime import datetime
+import subprocess
 
 api_token = 'place_the_token_here'
 
@@ -27,7 +28,9 @@ def save_data_to_file(data):
         row.append(repository['node']['name'])
         row.append(repository['node']['url'])
         age = calculate_repository_age(repository['node']['createdAt'])
-        row.append(math.modf(age))
+        age = math.modf(age)
+        age = age[1]
+        row.append(age)
         row.append(repository['node']['releases']['totalCount'])
         writer.writerow(row)
     f.close()
@@ -93,10 +96,48 @@ for x in range(1, 100):
     query = query.replace(old_end_cursor, new_end_cursor)
     old_end_cursor = new_end_cursor
 
+today = date.today()
+with open('repositories.csv', newline='') as csvfile:
+  spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+  next(spamreader)
+  for row in spamreader:
+    name = row[0].split(',')[0]
+    repo_clone_url = row[0].split(',')[1]
+    age = row[0].split(',')[2]
+    releases_number = row[0].split(',')[3]
+    break
 
-Repo.clone_from("https://github.com/iluwatar/java-design-patterns", "./repository")
+# Repo.clone_from(repo_clone_url, "./repository")
+Repo.clone_from(repo_clone_url, "./repository")
+
+# Run CK
+subprocess.call(['java', '-jar', 'ck-ck-0.7.0/target/ck-0.7.0-jar-with-dependencies.jar', './repository'])
+
+# Get repository CK metrics
+with open('class.csv', newline='') as csvfile:
+  spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+  next(spamreader)
+  loc_total = 0
+  cbo_total  = 0
+  dit_total  = 0
+  lcon_total  = 0
+  for row in spamreader:
+    loc = row[0].split(',')[34]
+    cbo = row[0].split(',')[3]
+    dit = row[0].split(',')[8]
+    lcon = row[0].split(',')[11]
+
+    loc_total += int(loc) 
+    cbo_total += int(cbo) 
+    dit_total += int(dit) 
+    lcon_total += int(lcon)
 
 
-
-
+header = ['name', 'repo_clone_url', 'age', 'releases_number', 'size', 'CBO', 'DIT', 'LCOM']
+row = [name, repo_clone_url, age, releases_number, loc_total, cbo_total, dit_total, lcon_total]
+f = open('result.csv', 'w', newline='')
+writer = csv.writer(f)
+writer.writerow(header)
+writer.writerow(row)
+f.close()
 
